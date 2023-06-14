210978a3a457ed6eba8cc1da1d9519fa370954a5
mechkouri <saidmechkouri@gmail.com>
2023-06-14 17:13:08 +0100

    search function aded

--- moviespace/src/App.js
+++ moviespace/src/App.js
@@ -1,57 +1,97 @@
 import './App.css';
-import { useCallback, useEffect } from 'react';
-import logo from './logo.svg'
-
-//45e86c82
+import { useEffect, useState } from 'react';
+import logo from './logo.svg';
+import MovieCard from './MovieCard';
 
 const API_URL = 'http://www.omdbapi.com/?i=tt3896198&apikey=45e86c82';
-const movie1 = {
-  "Title": "Guardians of the Galaxy Vol. 2",
-  "Year": "2017",
-  "imdbID": "tt3896198",
-  "Type": "movie",
-  "Poster": "https://m.media-amazon.com/images/M/MV5BNjM0NTc0NzItM2FlYS00YzEwLWE0YmUtNTA2ZWIzODc2OTgxXkEyXkFqcGdeQXVyNTgwNzIyNzg@._V1_SX300.jpg"
-}
 
 const App = () => {
-  const searchMovies = async (title) => {
-      const response = await fetch(`${API_URL}&S=${title}`);
-      const data = await response.json();
+  const [movies, setMovies] = useState([]);
+  const [searchTerm, setSearchTerm] = useState('');
+  const [searchSuggestions, setSearchSuggestions] = useState([]);
 
-      console.log(data.Search);
-      }
-  useEffect(()=>{
-    searchMovies('Guardians of the Galaxy Vol. 2')
-  },[] )
+  const searchMovies = async (title) => {
+    const response = await fetch(`${API_URL}&s=${title}`);
+    const data = await response.json();
+
+    if (data.Response === 'True') {
+      setMovies(data.Search);
+    } else {
+      setMovies([]);
+    }
+  };
+
+  const handleSearch = () => {
+    searchMovies(searchTerm);
+  };
+
+  const handleAutocomplete = async (title) => {
+    setSearchTerm(title);
+    setSearchSuggestions([]);
+    await searchMovies(title);
+  };
+
+  const handleInputChange = (e) => {
+    const value = e.target.value;
+    setSearchTerm(value);
+
+    if (value.length > 0) {
+      searchAutocomplete(value);
+    } else {
+      setSearchSuggestions([]);
+    }
+  };
+
+  const searchAutocomplete = async (title) => {
+    const response = await fetch(`${API_URL}&s=${title}`);
+    const data = await response.json();
+
+    if (data.Response === 'True') {
+      setSearchSuggestions(data.Search.map((movie) => movie.Title));
+    } else {
+      setSearchSuggestions([]);
+    }
+  };
+
+  useEffect(() => {
+    searchMovies('Spiderman');
+  }, []);
 
   return (
     <div className='app'>
       <h1>MovieSpace</h1>
       <div className='search'>
         <input
-          placeholder='Srearch for movies'
-          value="Superman"
-          onChange={()=> {}}
+          placeholder='Search for movies'
+          value={searchTerm}
+          onChange={handleInputChange}
         />
-        <img 
-          src={logo}
-          alt='search'
-          onClick={()=> {}}
-        />
-      </div>
-      <div className="container">
-        <div className="movie">
-          <div>
-            <p>{movie1.Year}</p>
-          </div>
-          <div>
-              <img src={movie1.Poster !== 'N/A' ? movie1.Poster : "https://via.placeholder.com/400"} alt={movie1.Title}  />
-          </div>
-        </div>
+        <img src={logo} alt='search' onClick={handleSearch} />
       </div>
 
+      {searchSuggestions.length > 0 && (
+        <ul className='suggestions'>
+          {searchSuggestions.map((suggestion) => (
+            <li key={suggestion} onClick={() => handleAutocomplete(suggestion)}>
+              {suggestion}
+            </li>
+          ))}
+        </ul>
+      )}
+
+      {movies?.length > 0 ? (
+        <div className='container'>
+          {movies.map((movie) => (
+            <MovieCard movie={movie} key={movie.imdbID} />
+          ))}
+        </div>
+      ) : (
+        <div className='empty'>
+          <h2>No movies found</h2>
+        </div>
+      )}
     </div>
   );
-}
+};
 
 export default App;
